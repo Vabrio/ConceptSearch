56b5f5593db9af323479db9e59af6d27ac67c073
Nathanaël <nathanael.eon@centrale-marseille.fr>
2017-03-04 17:41:54 +0100

    renommé :         server/CS.sqlite -> server/db/CS.sqlite 	modifié :         server/db/db_creation.js

--- server/db/db_creation.js
+++ server/db/db_creation.js
@@ -5,12 +5,13 @@ var SQL = require('sql.js');
 var db = new SQL.Database();
 // Run a query without reading the results
 db.run("CREATE TABLE wri_writings (id integer primary key, adress text, name text, writer text);");
-db.run("CREATE TABLE con_concepts (id integer primary key, conceptname text, writingid integer, begin integer, end integer, verse integer, userids integer[], strength integer);");
+db.run("CREATE TABLE con_concepts_draft (id integer primary key, name text, writingid integer, begin integer, end integer, userid text, strength integer);");
+db.run("CREATE TABLE con_concepts_ordered (id integer primary key, name text, writingid integer, begin integer, end integer, force integer);");
 db.run("CREATE TABLE gra_graphe_concepts (conceptname text, neighbors text[]);");
 
-/*db.run("INSERT INTO wri_writings (adress, name, writer) VALUES (?,?,?)", 
-       ["Library/French/Baha'i/Baha'u'llah/Paroles Cachees.txt","Paroles Cachées","Baha'u'llah"]);*/
+db.run("INSERT INTO wri_writings (adress, name, writer) VALUES (?,?,?)", 
+       ["../Library/French/Baha'i/Baha'u'llah/Paroles Cachees.txt","Paroles Cachées","Baha'u'llah"]);
 
 var data = db.export();
 var buffer = new Buffer(data);
-fs.writeFileSync("../CS.sqlite", buffer);
+fs.writeFileSync("CS.sqlite", buffer);
