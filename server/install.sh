81bd8aa7da1b9ff8df8a738b17a156fe5fb7b68d
Nathanaël EON <neon@ec-m.fr>
2018-03-01 00:54:45 +0100

    Updated the installation files 	modifié :         server/install.sh 	modifié :         server/package.json

--- server/install.sh
+++ server/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-npm install tsc
+npm install typescript
 npm install iconv-lite
 npm install express
 npm install fs
 npm install mysql
+npm install bcrypt
+npm install body-parser
+npm install jsonwebtoken
 
 mysql -u conceptsearch -ppassword -e "create database cs;"
