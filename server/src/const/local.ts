0fe0fd7d4562b22001b178c040d9bb9b137c1629
Nathanael <nath@vps485010.ovh.net>
2018-03-03 09:28:27 +0100

    Changed local.ts 	modified:   local.ts

--- server/src/const/local.ts
+++ server/src/const/local.ts
@@ -1,6 +1,6 @@
-const PORT = 8080;
-const SSL_KEY = "path_to_key";
-const SSL_CERTIFICATE = "path_to_certificate";
-const USE_SSL = false;
+const PORT = 8082;
+const SSL_KEY = "/etc/letsencrypt/live/concept-search.org/privkey.pem";
+const SSL_CERTIFICATE = "/etc/letsencrypt/live/concept-search.org/fullchain.pem";
+const USE_SSL = true;
 
 export { PORT, SSL_KEY, SSL_CERTIFICATE, USE_SSL}
