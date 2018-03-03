6bdb9f8c9e57354496e7c33bc6d1eae392ecc6c0
Nathanaël EON <neon@ec-m.fr>
2018-03-03 14:47:19 +0100

    Restricted files updated 	nouveau fichier : client/js/version.js 	nouveau fichier : server/src/const/local.ts

---
+++ client/js/version.js
@@ -0,0 +1,3 @@
+var version="DEV"; // "PROD" is the one for the server
+var ssl_status="http";
+var PORT = 8080;
---
+++ server/src/const/local.ts
@@ -0,0 +1,6 @@
+const PORT = 8080;
+const SSL_KEY = "path_to_ssl_key";
+const SSL_CERTIFICATE = "path_to_ssl_certificate";
+const USE_SSL = false;
+
+export { PORT, SSL_KEY, SSL_CERTIFICATE, USE_SSL}
