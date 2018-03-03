5da84608512b16c8300ee70581b30fa66df34a97
Nathanaël EON <neon@ec-m.fr>
2018-03-03 10:03:12 +0100

    Changed local 	supprimé :        server/src/const/local.ts

--- server/src/const/local.ts
+++
@@ -1,6 +0,0 @@
-const PORT = 8082;
-const SSL_KEY = "/etc/letsencrypt/live/concept-search.org/privkey.pem";
-const SSL_CERTIFICATE = "/etc/letsencrypt/live/concept-search.org/fullchain.pem";
-const USE_SSL = true;
-
-export { PORT, SSL_KEY, SSL_CERTIFICATE, USE_SSL}
