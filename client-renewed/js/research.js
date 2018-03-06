463a93e0eb06822dba51e50959049331b28034d1
Nathanaël EON <neon@ec-m.fr>
2018-03-06 10:44:01 +0100

    Single-page app : merged last page 	renommé :         client-renewed/css/stylesheetindex.css -> client-renewed/css/custom.css 	supprimé :        client-renewed/css/stylesheetsearch.css 	modifié :         client-renewed/index.html 	nouveau fichier : client-renewed/js/research.js 	supprimé :        client-renewed/recherche1.html

---
+++ client-renewed/js/research.js
@@ -0,0 +1,36 @@
+function searchRes(ans){
+	homePage.homePage= false;
+}
+
+var homePage = new Vue({
+	el: "#homePage",
+	data: {
+		homePage: true,
+		searchbar: "",
+	},
+	computed: {
+	},
+	methods: {
+		search: function(){
+			console.log("searching...");
+			searchRes(null);
+		}
+	}
+})
+
+var researchPage = new Vue({
+	el: "#researchPage",
+	data: {
+		searchbar: ""
+	},
+	computed: {
+		researchPage: function(){
+			return !homePage.homePage;	
+		}
+	},
+	methods:{
+		toggleHomePage: function(){
+			homePage.homePage= true;	
+		},
+	}
+})
