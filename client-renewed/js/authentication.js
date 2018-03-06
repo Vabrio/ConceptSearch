ae55ab2a76a7b58e6c8a6f1a283b7ed5bacbac16
Nathanaël EON <neon@ec-m.fr>
2018-03-06 07:55:47 +0100

    small updates on graphics and interaction login 	modifié :         client-renewed/index.html 	modifié :         client-renewed/js/authentication.js

--- client-renewed/js/authentication.js
+++ client-renewed/js/authentication.js
@@ -41,20 +41,32 @@ var logger = new Vue({
 		pseudo: "",
 		password: "",
 		password_check: "",
+		email: "",
+		email_check: "",
 		firstname: "",
 		lastname: "",
-		email: "",
 		birth_date: "",
 		state_login: "",
-		state_pwd: ""
+		state_pwd: "",
+		state_email: ""
 	},
 	computed: {
+	},
+	watch:{
 	},
 	methods: {
 		toggleConnect: function(){
 			this.subscribing = !this.subscribing;
 			this.state_login ="";
 			this.state_pwd ="";
+		},
+		pwdC: function(){
+			if (this.password_check == this.password){this.state_pwd = ""}
+			else {this.state_pwd = "has-warning"}
+		},
+		emailC: function(){
+			if (this.email_check == this.email){this.state_email = ""}
+			else {this.state_email = "has-warning"}
 		},
 		submit: function(){
 			if (this.subscribing){this.subscribe()}
@@ -68,6 +80,8 @@ var logger = new Vue({
 		subscribe: function(){
 			if (this.password != this.password_check){
 				this.state_pwd = "has-error";
+			}else if (this.email != this.email_check){
+				this.state_email = "has-error";
 			}else {
 				this.$http.post(url+"users/subscribe?name="+ this.pseudo +"&password="+this.password).then(response => {
 					subscribed(response);
